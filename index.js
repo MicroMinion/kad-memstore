f6017a2eda22eac1d378334b67660011126217c0
Thomas Delaet <thomas@delaet.org>
2016-09-09 20:23:00 +0200

    standard formatting

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
  * @module kad-memstore
  */
 
-'use strict';
+'use strict'
 
-var ReadableStream = require('readable-stream');
-var assert = require('assert');
-var _ = require('lodash');
-var setImmediate = require('async.util.setimmediate');
+var ReadableStream = require('readable-stream')
+var assert = require('assert')
+var _ = require('lodash')
+var setImmediate = require('async.util.setimmediate')
 
 /**
  * Creates an in-memory kad storage adapter
  * @constructor
  */
-function KadMemStore() {
+function KadMemStore () {
   if (!(this instanceof KadMemStore)) {
-    return new KadMemStore();
+    return new KadMemStore()
   }
 
-  this._store = {};
+  this._store = {}
 }
 
 /**
@@ -27,18 +27,18 @@ function KadMemStore() {
  * @param {string} key
  * @param {function} callback
  */
-KadMemStore.prototype.get = function(key, callback) {
+KadMemStore.prototype.get = function (key, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(_.isFunction(callback), 'callback is not a valid function')
-  var self = this;
-  setImmediate(function() {
-    if(self._store[key]) {
+  var self = this
+  setImmediate(function () {
+    if (self._store[key]) {
       callback(null, self._store[key])
     } else {
       callback(new Error('Key not found'))
     }
-  });
-};
+  })
+}
 
 /**
  * Puts an item into the store
@@ -47,15 +47,15 @@ KadMemStore.prototype.get = function(key, callback) {
  * @param {string} value
  * @param {function} callback
  */
-KadMemStore.prototype.put = function(key, value, callback) {
+KadMemStore.prototype.put = function (key, value, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(!callback || _.isFunction(callback), 'callback is not a valid function')
   assert(_.isString(value), 'value is not a valid string')
-  this._store[key] = value;
-  if(callback) {
-    setImmediate(callback);
+  this._store[key] = value
+  if (callback) {
+    setImmediate(callback)
   }
-};
+}
 
 /**
  * Deletes an item from the store
@@ -63,47 +63,47 @@ KadMemStore.prototype.put = function(key, value, callback) {
  * @param {string} key
  * @param {function} callback
  */
-KadMemStore.prototype.del = function(key, callback) {
+KadMemStore.prototype.del = function (key, callback) {
   assert(_.isString(key), 'key is not a valid string')
   assert(!callback || _.isFunction(callback), 'callback is not a valid function')
-  if(!callback) {
-    callback = function() {}
+  if (!callback) {
+    callback = function () {}
   }
-  setImmediate(function() {
-    if(this._store[key]) {
+  setImmediate(function () {
+    if (this._store[key]) {
       delete this._store[key]
       callback()
     } else {
       callback(new Error('Key not found'))
     }
   })
-};
+}
 
 /**
  * Returns a readable stream of items
  * #createReadStream
  */
-KadMemStore.prototype.createReadStream = function() {
-  var adapter = this;
-  var items = Object.keys(this._store);
-  var current = 0;
+KadMemStore.prototype.createReadStream = function () {
+  var adapter = this
+  var items = Object.keys(this._store)
+  var current = 0
 
   return new ReadableStream({
     objectMode: true,
-    read: function() {
-      var stream = this;
-      var key = items[current];
+    read: function () {
+      var stream = this
+      var key = items[current]
 
       if (!key) {
-        return stream.push(null);
+        return stream.push(null)
       }
 
-      setImmediate(function pushItem() {
-        current++;
-        stream.push({ key: key, value: adapter._store[key] });
-      });
+      setImmediate(function pushItem () {
+        current++
+        stream.push({ key: key, value: adapter._store[key] })
+      })
     }
-  });
-};
+  })
+}
 
-module.exports = KadMemStore;
+module.exports = KadMemStore
--- test/memstore.unit.js
+++ test/memstore.unit.js
@@ -1,96 +1,96 @@
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var KadMemStore = require('..');
+var expect = require('chai').expect
+var KadMemStore = require('..')
 
-describe('KadMemStore', function() {
+describe('KadMemStore', function () {
 
-  describe('@constructor', function() {
+  describe('@constructor', function () {
 
-    it('should create an instance without the new keyword', function() {
-      expect(KadMemStore()).to.be.instanceOf(KadMemStore);
-    });
+    it('should create an instance without the new keyword', function () {
+      expect(KadMemStore()).to.be.instanceOf(KadMemStore)
+    })
 
-    it('should create an instance with the new keyword', function() {
-      expect(new KadMemStore()).to.be.instanceOf(KadMemStore);
-    });
+    it('should create an instance with the new keyword', function () {
+      expect(new KadMemStore()).to.be.instanceOf(KadMemStore)
+    })
 
-  });
+  })
 
-  describe('#get', function() {
+  describe('#get', function () {
 
-    it('should return the value at _store[key]', function(done) {
-      var store = new KadMemStore();
+    it('should return the value at _store[key]', function (done) {
+      var store = new KadMemStore()
       store._store.beep = JSON.stringify({
         key: 'beep',
         value: 'boop',
         publisher: 'c9ddf842c1acb00c2660f47922da1428e49eb843',
         timestamp: Date.now()
-      });
-      store.get('beep', function(err, value) {
-        expect(value).to.equal(store._store.beep);
-        done();
-      });
-    });
+      })
+      store.get('beep', function (err, value) {
+        expect(value).to.equal(store._store.beep)
+        done()
+      })
+    })
 
-    it('should return null if no item at key', function(done) {
-      var store = new KadMemStore();
-      store.get('beep', function(err, value) {
-        expect(value).to.equal(null);
-        done();
-      });
-    });
+    it('should return null if no item at key', function (done) {
+      var store = new KadMemStore()
+      store.get('beep', function (err, value) {
+        expect(value).to.equal(null)
+        done()
+      })
+    })
 
-  });
+  })
 
-  describe('#put', function() {
+  describe('#put', function () {
 
-    it('should put the value at _store[key]', function(done) {
-      var store = new KadMemStore();
+    it('should put the value at _store[key]', function (done) {
+      var store = new KadMemStore()
       var item = JSON.stringify({
         key: 'beep',
         value: 'boop',
         publisher: 'c9ddf842c1acb00c2660f47922da1428e49eb843',
         timestamp: Date.now()
-      });
-      store.put('beep', item, function(err) {
-        expect(store._store.beep).to.equal(item);
-        done();
-      });
-    });
+      })
+      store.put('beep', item, function (err) {
+        expect(store._store.beep).to.equal(item)
+        done()
+      })
+    })
 
-  });
+  })
 
-  describe('#del', function() {
+  describe('#del', function () {
 
-    it('should delete the value at _store[key]', function(done) {
-      var store = new KadMemStore();
+    it('should delete the value at _store[key]', function (done) {
+      var store = new KadMemStore()
       store._store.beep = JSON.stringify({
         key: 'beep',
         value: 'boop',
         publisher: 'c9ddf842c1acb00c2660f47922da1428e49eb843',
         timestamp: Date.now()
-      });
-      store.del('beep', function(err) {
-        expect(store._store.beep).to.equal(undefined);
-        done();
-      });
-    });
+      })
+      store.del('beep', function (err) {
+        expect(store._store.beep).to.equal(undefined)
+        done()
+      })
+    })
 
-  });
+  })
 
-  describe('#createReadStream', function() {
+  describe('#createReadStream', function () {
 
-    it('should stream the keys in the proper format then end', function(done) {
-      var store = new KadMemStore();
+    it('should stream the keys in the proper format then end', function (done) {
+      var store = new KadMemStore()
 
-      function createItem(key, value) {
+      function createItem (key, value) {
         return JSON.stringify({
           key: key,
           value: value,
           publisher: 'c9ddf842c1acb00c2660f47922da1428e49eb843',
           timestamp: Date.now()
-        });
+        })
       }
 
       store._store = {
@@ -104,23 +104,23 @@ describe('KadMemStore', function() {
         eight: createItem('eight', '8'),
         nine: createItem('nine', '9'),
         ten: createItem('ten', '10')
-      };
+      }
 
-      var stream = store.createReadStream();
+      var stream = store.createReadStream()
 
-      stream.on('data', function(data) {
-        expect(typeof data.key).to.equal('string');
-        expect(typeof data.value).to.equal('string');
-        expect(Object.keys(JSON.parse(data.value)).length).to.equal(4);
-        store.del(data.key, function(err) {
-          expect(err).to.equal(undefined);
-        });
-      }).on('end', function() {
-        expect(Object.keys(store._store).length).to.equal(0);
-        done();
-      });
-    });
+      stream.on('data', function (data) {
+        expect(typeof data.key).to.equal('string')
+        expect(typeof data.value).to.equal('string')
+        expect(Object.keys(JSON.parse(data.value)).length).to.equal(4)
+        store.del(data.key, function (err) {
+          expect(err).to.equal(undefined)
+        })
+      }).on('end', function () {
+        expect(Object.keys(store._store).length).to.equal(0)
+        done()
+      })
+    })
 
-  });
+  })
 
-});
+})
